acdbe5f446913b8fca94e3c5c5627d0a74cfba2b
Evgeny Parkhomenko <evg.parkhomenko@gmail.com>
2024-01-21 14:35:53 +0300

    feat: implement task 01-read-file

---
+++ 01-read-file/index.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const path = require('path');
+
+const pathTextFile = path.join(__dirname, 'text.txt');
+const readStream = fs.createReadStream(pathTextFile, 'utf-8');
+
+let data = '';
+
+readStream.on('data', (chunk) => (data += chunk));
+readStream.on('end', () => console.log(data));
+readStream.on('error', (error) => console.log('Error', error.message));
